45c394e41897f4da09c234811b8a57f8ef6bf80c
JaimeCs27 <jaimecab2002@gmail.com>
2024-05-13 10:52:34 -0600

    a

--- routes/AsistenteAdministrativoRoutes.js
+++ routes/AsistenteAdministrativoRoutes.js
@@ -157,11 +157,11 @@ router.post("/DarDeBaja", async (req, res) => {
   const prof = await team.findOneAndDelete({
     "profesor.campus": req.body.campus,
   });
-  console.log(prof)
+
   const profe = await user.findByIdAndUpdate(prof.profesor.id, {
     $set: { isCoord: false },
   }, {new: true});
-  console.log(profe)
+
   if (profe) {
     res.send("Success");
   } else {
